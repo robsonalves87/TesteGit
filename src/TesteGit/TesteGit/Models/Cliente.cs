fde501ef151378cd50702ddbcc2b994cc4c04078
Robson Alves <robsonalves87@live.com>
2020-01-31 23:15:16 -0300

    Commit 2

--- src/TesteGit/TesteGit/Models/Cliente.cs
+++ src/TesteGit/TesteGit/Models/Cliente.cs
@@ -1,11 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace TesteGit.Models
 {
     public class Cliente : Entity
     {
+        public Cliente(Guid id, string nome, string email, DateTime dataNascimento)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            DataNascimento = dataNascimento;
+        }
+
+        // Empty constructor for EF
+        protected Cliente() { }
+
+        public string Nome { get; private set; }
+
+        public string Email { get; private set; }
+
+        public DateTime DataNascimento { get; private set; }
     }
 }
